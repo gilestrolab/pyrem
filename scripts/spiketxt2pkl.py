d2b03fcdf2aa7cedf0fd3947c995417254f24ee6
Quentin Geissmann <qgeissmann@gmail.com>
2014-07-08 14:59:48 +0100

    add a scrpt dir -> parsing data and stuff

--- scripts/spiketxt2pkl.py
+++ scripts/spiketxt2pkl.py
@@ -1,1 +1,20 @@
 __author__ = 'quentin'
+
+import sys
+import numpy as np
+import pandas as pd
+import pyrem as pr
+
+CHANNELS = ['e','e','m','m']
+METADATA = {"organism":"Mus musculus", "sex":"M", "author":"Valentina Ferretti"}
+SAMPLING_FREQ = 200
+
+if __name__ == "__main__":
+    input = sys.argv[1]
+    output = sys.argv[2]
+    df = pd.read_csv(input, sep="\t")
+    df = np.array(df.drop("Time",axis=1).dropna())
+    met = METADATA
+    met["raw_file_name"] = input
+    sig = pr.Signal(df,SAMPLING_FREQ, metadata=METADATA, channel_types=CHANNELS, normalise=False)
+    sig.save(output)
