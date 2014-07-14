369e1d9dd7276f42210649f26a373445ab81c5d0
Quentin Geissmann <qgeissmann@gmail.com>
2014-07-14 19:28:47 +0100

    doccument and fix features

--- src/pyrem/features/univariate.py
+++ src/pyrem/features/univariate.py
@@ -1,76 +1,23 @@
 __author__ = 'quentin'
-
 import numpy as np
 
-
-def _embed_seq(X,Tau,D):
+def _embed_seq(X,tau,de):
 
     N =len(X)
 
-    if D * Tau > N:
+    if de * tau > N:
         raise ValueError("Cannot build such a matrix, because D * Tau > N")
 
-    if Tau<1:
+    if tau<1:
         raise ValueError("Tau has to be at least 1")
 
 
-    Y=np.zeros((D, N - (D - 1) * Tau))
+    Y=np.zeros((de, N - (de - 1) * tau))
 
-    for i in range(D):
-        Y[i] = X[i *Tau : i*Tau + Y.shape[1] ]
+    for i in range(de):
+        Y[i] = X[i *tau : i*tau + Y.shape[1] ]
 
     return Y.T
-
-def pfd(X):
-    """
-    Compute Petrosian Fractal Dimension of a time series.
-    Vectorised/optimised version of the eponymous function from PyEEG (https://code.google.com/p/pyeeg/).
-
-    :param X: a one dimensional array representing a time series
-    :type X: np.ndarray
-    :return: the Petrosian Fractal Dimension; a scalar.
-    :rtype: float
-    """
-    diff = np.diff(X)
-    # x[i] * x[i-1] for i in t0 -> tmax
-    prod = diff[1:-1] * diff[0:-2]
-
-    # number of sign changes in derivative of the signal
-    N_delta = np.sum(prod < 0)
-    n = len(X)
-    return np.log10(n)/(np.log10(n)+np.log10(n/n+0.4*N_delta))
-
-def hjorth(X):
-
-
-    first_deriv = np.diff(X)
-    second_deriv = np.diff(X,2)
-
-    var_zero = np.mean(X ** 2)
-    var_d1 = np.mean(first_deriv ** 2)
-    var_d2 = np.mean(second_deriv ** 2)
-
-
-    activity = var_zero ** 2.0
-    morbidity = var_d1 / var_zero
-    complexity = np.abs(var_d2 / var_d1) - morbidity ** 2.0
-
-    return activity, morbidity, complexity
-
-
-def svd_entropy(X, Tau, DE):
-    mat =  _embed_seq(X, Tau, DE)
-    W = np.linalg.svd(mat, compute_uv = False)
-    W /= sum(W) # normalize singular values
-    return -1*sum(W * np.log(W))
-
-def fisher_info(X, Tau, DE):
-    mat =  _embed_seq(X, Tau, DE)
-    W = np.linalg.svd(mat, compute_uv = False)
-    W /= sum(W) # normalize singular values
-    FI_v = (W[1:] - W[:-1]) **2 / W[:-1]
-
-    return np.sum(FI_v)
 
 def _make_cmp(X, M, R, in_range_i, in_range_j, ap_ent=True):
      #Then we make Cmp
@@ -126,28 +73,240 @@ def _make_cm(X,M,R,ap_ent=True):
 
         return Cm.astype(np.float), in_range_i, in_range_j
 
-def ap_entropy(X, M, R):
-    N = len(X)
-    Cm, in_range_i, in_range_j = _make_cm(X,M,R)
+def pfd(a):
+    r"""
+    Compute Petrosian Fractal Dimension of a time series [PET95]_.
 
-    Cmp = _make_cmp(X, M, R, in_range_i, in_range_j)
+    It is defined by:
 
-    Cm /= float((N - M +1 ))
-    Cmp /= float(N - M)
+    .. math::
+
+        \frac{log(N)}{log(N) + log(\frac{N}{N+0.4N_{\delta}})}
+
+
+    Where:
+
+    :math:`N` is the length of the time series, and
+
+    :math:`N_{\delta}` is the number of sign changes.
+
+
+    .. [PET95]  A. Petrosian, Kolmogorov complexity of finite sequences and recognition of different preictal EEG patterns, in ,
+        Proceedings of the Eighth IEEE Symposium on Computer-Based Medical Systems, 1995, 1995, pp. 212-217.
+
+
+
+    :param a: a one dimensional array representing a time series
+    :type a: np.ndarray
+    :return: the Petrosian Fractal Dimension; a scalar.
+    :rtype: float
+    """
+    diff = np.diff(a)
+    # x[i] * x[i-1] for i in t0 -> tmax
+    prod = diff[1:-1] * diff[0:-2]
+
+    # Number of sign changes in derivative of the signal
+    N_delta = np.sum(prod < 0)
+    n = len(a)
+    return np.log(n)/(np.log(n)+np.log(n/(n+0.4*N_delta)))
+
+def hjorth(X):
+    r"""
+    Compute Hjorth parameters [HJO70]_.
+
+
+    .. math::
+
+        Activity = m_0 = \sigma_{a}^2
+
+    .. math::
+
+        Morbidity = m_2 = \sigma_{d}/ \sigma_{a}
+
+    .. math::
+        Morbidity = m_4 =  \frac{\sigma_{dd}/ \sigma_{d}}{m_2}
+
+
+    Where:
+
+    :math:`\sigma_{x}^2` is the mean power of a signal :math:`x`. That is, its variance, if it's mean is zero.
+
+    :math:`a`, :math:`d` and :math:`dd` represent the original signal, its first and second derivatives, respectively.
+
+
+    .. [HJO70] B. Hjorth, EEG analysis based on time domain properties,
+        Electroencephalography and Clinical Neurophysiology, vol. 29, no. 3, pp. 306-310, Sep. 1970.
+
+    :param a: a one dimensional array representing a time series
+    :type a: np.ndarray
+    :return: activity, complexity and morbidity
+    :rtype: tuple(float, float, float)
+    """
+
+    first_deriv = np.diff(X)
+    second_deriv = np.diff(X,2)
+
+    var_zero = np.mean(X ** 2)
+    var_d1 = np.mean(first_deriv ** 2)
+    var_d2 = np.mean(second_deriv ** 2)
+
+
+    activity = var_zero
+    morbidity = np.sqrt(var_d1 / var_zero)
+    complexity = np.sqrt(var_d2 / var_d1) / morbidity
+
+    return activity, morbidity, complexity
+
+def svd_entropy(a, tau, de):
+    r"""
+    Compute the Single Value Decomposition entropy of a signal with embedding dimension "de" and delay "tau" [PYEEG]_.
+    The result differs from PyEEG implementation because :math:`log_2` is used, according to the definition in the paper.
+
+    .. [PYEEG] F. S. Bao, X. Liu, and C. Zhang, PyEEG: An Open Source Python Module for EEG/MEG Feature Extraction,
+    Computational Intelligence and Neuroscience, vol. 2011, p. e406391, Mar. 2011.
+
+    :param a: a one dimensional array representing a time series
+    :type a: np.ndarray
+    :param tau: the delay
+    :type tau: int
+    :param de: the embedding dimension
+    :type de: int
+    :return: the SVD entropy, a scalar
+    :rtype: float
+    """
+
+    mat =  _embed_seq(a, tau, de)
+    W = np.linalg.svd(mat, compute_uv = False)
+    W /= sum(W) # normalize singular values
+    return -1*sum(W * np.log2(W))
+
+def fisher_info(a, tau, de):
+    r"""
+    Compute the Fisher information of a signal with embedding dimension "de" and delay "tau" [PYEEG]_.
+    Vectorised version of the PyEEG function.
+
+    .. [PYEEG] F. S. Bao, X. Liu, and C. Zhang, PyEEG: An Open Source Python Module for EEG/MEG Feature Extraction,
+    Computational Intelligence and Neuroscience, vol. 2011, p. e406391, Mar. 2011.
+
+    :param a: a one dimensional array representing a time series
+    :type a: np.ndarray
+    :param tau: the delay
+    :type tau: int
+    :param de: the embedding dimension
+    :type de: int
+    :return: the Fisher information, a scalar
+    :rtype: float
+    """
+
+    mat =  _embed_seq(a, tau, de)
+    W = np.linalg.svd(mat, compute_uv = False)
+    W /= sum(W) # normalize singular values
+    FI_v = (W[1:] - W[:-1]) **2 / W[:-1]
+
+    return np.sum(FI_v)
+
+def ap_entropy(a, m, R):
+    r"""
+    Compute the approximate entropy of a signal with embedding dimension "de" and delay "tau" [PYEEG]_.
+    Vectorised version of the PyEEG function. Faster than PyEEG, but still critically slow.
+
+    .. [PYEEG] F. S. Bao, X. Liu, and C. Zhang, PyEEG: An Open Source Python Module for EEG/MEG Feature Extraction,
+    Computational Intelligence and Neuroscience, vol. 2011, p. e406391, Mar. 2011.
+
+    :param a: a one dimensional array representing a time series
+    :type a: np.ndarray
+    :param m: the scale
+    :type m: int
+    :param R: The tolerance
+    :type R: float`
+    :return: the approximate entropy, a scalar
+    :rtype: float
+    """
+
+    N = len(a)
+    Cm, in_range_i, in_range_j = _make_cm(a,m,R)
+
+    Cmp = _make_cmp(a, m, R, in_range_i, in_range_j)
+
+    Cm /= float((N - m +1 ))
+    Cmp /= float(N - m)
 
     Phi_m, Phi_mp = np.sum(np.log(Cm)),  np.sum(np.log(Cmp))
-    Ap_En = (Phi_m - Phi_mp) / (N - M)
+    Ap_En = (Phi_m - Phi_mp) / (N - m)
     return Ap_En
 
+def samp_entropy(a, m, R):
+    r"""
+    Compute the sample entropy of a signal with embedding dimension "de" and delay "tau" [PYEEG]_.
+    Vectorised version of the PyEEG function. Faster than PyEEG, but still critically slow.
 
-def samp_entropy(X, M, R):
+    .. [PYEEG] F. S. Bao, X. Liu, and C. Zhang, PyEEG: An Open Source Python Module for EEG/MEG Feature Extraction,
+    Computational Intelligence and Neuroscience, vol. 2011, p. e406391, Mar. 2011.
 
-    Cm, in_range_i, in_range_j = _make_cm(X,M,R, False)
-    Cmp = _make_cmp(X, M, R, in_range_i, in_range_j, False)
+    :param a: a one dimensional array representing a time series
+    :type a: np.ndarray
+    :param m: the scale
+    :type m: int
+    :param R: The tolerance
+    :type R: float`
+    :return: the approximate entropy, a scalar
+    :rtype: float
+    """
+
+
+    Cm, in_range_i, in_range_j = _make_cm(a,m,R, False)
+    Cmp = _make_cmp(a, m, R, in_range_i, in_range_j, False)
 
     Samp_En = np.log(sum(Cm)/sum(Cmp))
 
     return Samp_En
+
+
+def spectral_entropy(a, sampling_freq, bands=None):
+
+    r"""
+    Compute spectral entropy of a  signal with respect to frequency bands.
+    The power spectrum is computed through fft. Then, it is normalised and assimilated to a probability density function.
+    The entropy of the signal :math:`x` can be expressed by:
+
+    .. math::
+
+        H(x) =  -\sum_{f=0}^{f = f_s/2} PSD(f) log_2[PSD(f)]
+
+    Where:
+
+    :math:`PSD` is the normalised power spectrum (Power Spectrum Density), and
+
+    :math:`f_s` is the sampling frequency
+
+    :param a: a one dimensional array representing a time series
+    :type a: np.ndarray
+    :param sampling_freq: the sampling frequency
+    :type sampling_freq:  float
+    :param bands: a list of numbers delimiting the bins of the frequency bands. If None the entropy is computed over the whole range of the DFT (from 0 to :math:`f_s/2`)
+    :return: the spectral entropy; a scalar
+    """
+
+
+
+    psd = np.abs(np.fft.rfft(a))**2
+    psd /= np.sum(psd) # psd as a pdf (normalised to one)
+
+    if bands is None:
+        power_per_band= psd[psd>0]
+    else:
+        freqs = np.fft.rfftfreq(a.size, 1/float(sampling_freq))
+        bands = np.asarray(bands)
+
+        freq_limits_low = np.concatenate([[0.0],bands])
+        freq_limits_up = np.concatenate([bands, [np.Inf]])
+
+        power_per_band = [np.sum(psd[np.bitwise_and(freqs >= low, freqs<up)])
+                for low,up in zip(freq_limits_low, freq_limits_up)]
+
+        power_per_band= power_per_band[ power_per_band > 0]
+
+    return - np.sum(power_per_band * np.log2(power_per_band))
 
 
 def dfa(X, Ave = None, L = None, sampling= 1):
@@ -177,32 +336,6 @@ def dfa(X, Ave = None, L = None, sampling= 1):
 
     Alpha = np.polyfit(np.log(LF[0]), np.log(F),1)[0]
     return Alpha
-
-
-def spectral_entropy(signal, Band, sampling_freq):
-        """
-        Code adapted from Forrest Sheng Bao's pyeeg
-        """
-        X = signal
-        Fs = sampling_freq
-        C = np.fft.fft(X)
-        C = abs(C)
-        Power = np.zeros(len(Band)-1);
-        for Freq_Index in xrange(0,len(Band)-1):
-            Freq = float(Band[Freq_Index])                                        ## Xin Liu
-            Next_Freq = float(Band[Freq_Index+1])
-            Power[Freq_Index] = sum(C[int(Freq / Fs*len(X)):int(Next_Freq / Fs*len(X))])
-
-
-        Power_Ratio = Power/float(np.sum(Power))
-
-        Spectral_Entropy = 0
-        for i in xrange(0, len(Power_Ratio) - 1):
-            Spectral_Entropy += Power_Ratio[i] * np.log(Power_Ratio[i])
-        Spectral_Entropy /= np.log(len(Power_Ratio))    # to save time, minus one is omitted
-        return -1 * Spectral_Entropy
-
-
 
 def hurst(signal):
     """
@@ -296,7 +429,6 @@ def hfd(X, Kmax):
 
         L.append(np.log(np.mean(Lk)))
         x.append([np.log(float(1) / k), 1])
-    # print L,x
     (p, r1, r2, s)=np.linalg.lstsq(x, L)
     return p[0]
 
