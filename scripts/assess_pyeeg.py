84d9b6a0b4a16383a8b2891ca6011e045e011586
Quentin Geissmann <qgeissmann@gmail.com>
2014-07-15 10:56:29 +0100

    Update assess_pyeeg.py

--- scripts/assess_pyeeg.py
+++ scripts/assess_pyeeg.py
@@ -6,7 +6,7 @@ Issues with pyeeg:
 General: no use of vectoristation and high level numpy functions such as np.diff (instead of manual implementation of numerical derivatives)
 
 Specific:
-    * Hjorth parameters are not normalises by the length of the time series.
+    * Hjorth parameters are not normalised by the length of the time series.
 
     * HFD is for Higuchi, not Hjorth Fractal Dimention.
 
