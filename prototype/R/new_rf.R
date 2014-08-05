2d4eb22043e70a1b36a92b4b7585c261bc582062
Quentin Geissmann <qgeissmann@gmail.com>
2014-08-05 13:32:16 +0100

    R prototype to assess optimal lag (tau) for time time series forcasting using vicinity information

--- prototype/R/new_rf.R
+++ prototype/R/new_rf.R
@@ -5,63 +5,255 @@ printdbg <- function(){
     assign("DBG", DBG+1, envir = .GlobalEnv)
     
 }
+set.seed(1)
 
 library("randomForest")
+library("caret")
+library("abind")
+library("parallel")
 
 
 OUT_CSV = "/data/pyrem/Ellys/all_features.csv"
 
 curate_df <- function(dfo){
     df <- dfo
-    # remove erroneous kurtosis
-    #df <- subset(df, power_kurtosis != Inf)
-    #df <- subset(df, power_kurtosis != Inf)
-
-    # remove ambiguous vigilance states
-    df <- subset( df, vigilance_state.vigil.proba == 1)
-
-    df$vigilance_state.vigil.proba <- NULL
+    # remove cols containing NAs
+    s = sapply(df, function(col)sum(is.na(col)))
+    df <- df[,s ==0]
+    
+    
     df$t <- df$X
     df$X <- NULL
     df$animal <- sprintf("%s_%s", df$treatment, df$animal)
-    df <- subset(df, animal != "TelC_E")
     df$animal  <- as.factor(df$animal)
-
+    
+    
+    # fixme Hjorth activity == mean power
+    df  <- df[ ,grep("activity", colnames(df),invert=T)]
+    
+    # our Y variable is a factor (vigilance state)
     df$y <- as.factor(df[,"vigilance_state.vigil.value"])
     df$vigilance_state.vigil.value <- NULL
-    s = sapply(df, function(col)sum(is.na(col)))
-    df <- df[,s ==0]
+    df$py <- as.factor(df[,"vigilance_state.vigil.proba"])
+    df$vigilance_state.vigil.proba <- NULL
+    
+    # remove ambiguous vigilance states ?
+#~     df <- subset( df, vigilance_state.vigil.proba == 1)
+#~     df$vigilance_state.vigil.proba <- NULL 
+    
+    
     return(df)
 }
 
+#################################
+make_time_features <- function(lag, d, min_max_lag){
+    
+    # padding with vectro of NAs
+    min_lag <-  min_max_lag[1]
+    max_lag <- min_max_lag[2]
+    
+    range <- max_lag -  min_lag
+    
+    n_na_before <- range - (lag - min_lag) 
+    n_na_after <- range - n_na_before
+    
+    to_concatenate = list(NULL, NULL, NULL)
+    
+    if(n_na_before > 0)
+        to_concatenate[[1]] <- matrix(rep(NA, ncol(d)*n_na_before), nrow=n_na_before, dimnames=list(NULL, colnames(d)))
+    if(n_na_after > 0)
+        to_concatenate[[3]] <- matrix(rep(NA, ncol(d)*n_na_after), nrow=n_na_after, dimnames=list(NULL, colnames(d)))
+    
+    to_concatenate[[2]] <- d
+    out <- do.call("rbind", to_concatenate)
+    colnames(out) <- sprintf("lag_%s.%s", ifelse(lag<0, paste("m",abs(lag),sep=""), as.character(lag)), colnames(out))
+
+    return(out)
+    
+    }
+entropy <- function(v){
+    v[v==0] <- 1e-100
+    h <- 1 + sum(v * log2(v)) /log2(length(v))
+    return(h)
+}
+
+add_lagged_time_features <- function(d, pattern_valid_cols = "\\.", min_max_lag=c(-2,2)){
+    if (min_max_lag[2] == 0  & min_max_lag[1] ==0) 
+        return (d)
+    # we ensure data is sorted by time
+    d <- d[order(d$t),]
+    # we ensure we have an homogenous time series:
+    stopifnot(
+         length(unique(diff(d$t))) == 1
+    )
+    
+    cols_to_lag_idx <- grep(pattern_valid_cols, colnames(d))
+    
+    cols_to_lag <- d[,cols_to_lag_idx ]
+    
+    other_cols <- subset(d,select = -cols_to_lag_idx)
+    
+    min_lag <-  min_max_lag[1]
+    max_lag <- min_max_lag[2]
+    
+    stopifnot(min_lag < max_lag)
+    
+    
+    other_cols <- other_cols[(abs(min_lag) + 1): (nrow(other_cols) - abs(max_lag)),, drop=F]
+    
+    
+    dfs <- lapply(min_lag : max_lag, make_time_features, d=cols_to_lag, min_max_lag )
+    
+    all_lagged_cols <- na.omit(do.call("cbind",dfs))
+    
+    return(cbind(other_cols, all_lagged_cols))
+}
+
+
 ########################################################
 
-dfo <- read.csv(OUT_CSV , na.string="NaN")
-df <- curate_df(dfo)
+#~ 
 
 
 
 crossval_test <- function(out_level, original_df){
-        printdbg()
+        print(out_level)
         train_df <- subset(original_df, animal !=out_level)
         test_df <- subset(original_df, animal ==out_level)
         train_df$animal <- NULL
         test_df$animal <- NULL
-        printdbg()
-        rf <- randomForest(y ~ ., train_df, ntree=50)
-        printdbg()
-        print(rf)
-        varImpPlot(rf)
-        preds <- predict(rf, test_df)
-
+        
+        rf <- randomForest(y ~ ., train_df, ntree=50, sampsize=c(1000,1000,1000))        
+        
+        
+#~         varImpPlot(rf)       
+        preds <- predict(rf, test_df, type="prob")
+        pred_values <- apply(preds,1,function(v){colnames(preds)[which.max(v)]})
+        pred_values <- factor(pred_values, levels=levels(test_df$y))
         #out <- data.frame(real = test_df$y, preds = preds)
-        out <- sum(test_df$y == preds) / length(preds)
-        return(out)
+        out <- sum(test_df$y == pred_values) / length(pred_values)
+        
+        strt <- 500
+        stp<- 2000
+        l = stp - strt +1
+        h_rows <- apply(preds, 1, entropy)        
+        
+#~         plot( strt:stp, rep(1.2,l), col=as.numeric(test_df$y[strt:stp]) +2, pch=20,ylim=c(0,1.5))
+#~         
+#~         points( strt:stp, rep(1.3,l), col=as.numeric(pred_values[strt:stp]) +2, pch=20)
+#~         lines( strt:stp, h_rows[strt:stp])
+#~         points( strt:stp, h_rows[strt:stp], pch=20, col=ifelse(test_df$y[strt:stp] == pred_values[strt:stp], "blue", "red"))
+#~         
+        confidence <- 0:99/100
+        CV_accuracy <- sapply(confidence, function(r){
+                        valid <- (h_rows > r & h_rows <= r+0.1)
+                        sum_posit <- sum((test_df$y == pred_values)[valid])
+                        if (sum(valid) == 0)
+                            return(NA)
+                        return(sum_posit/sum(valid))
+                    }
+                    
+                )
+        print(out)
+        
+#~         plot(log10(CV_accuracy) ~ log10(confidence), type="b")
+        
+        return(confusionMatrix (pred_values, test_df$y))
+        
 }
 
-l = sapply(levels(df$animal), crossval_test, original_df=df)
-print(l)
-exit()
+select_variables <- function(df, t=0.75){
+    
+
+
+#~     pca <- princomp(~. , df[,grep("\\.", colnames(df))])
+#~     
+#~     components <- pca$scores
+#~     cumpca <- cumsum(pca$sdev)
+#~     cumpca <- cumpca - min(cumpca)
+#~     cumpca <- cumpca / max(cumpca)
+#~     components <- components[,1:which(cumpca > t)[1]]
+#~     
+#~     d_out <- df[, grep("\\.", colnames(df), invert=T)]
+#~     d_out <- cbind(data.frame(components), d_out)
+#~     d <- subset( d_out, py == 1)
+#~     d$py <- NULL 
+#~     d$treatment <- NULL 
+#~     d$t <- NULL 
+#~     d$animal <- NULL 
+#~     d$y <- droplevels(d$y)
+#~     rf <- randomForest(y ~ ., d, ntree=100, sampsize=c(1000,1000,1000))
+    #d <- subset( d, py == 1)
+#~     return(d_out)
+    
+#~     #remove ammbiguous vigilance states
+    d <- subset( df, py == 1)
+    d$py <- NULL 
+    d$treatment <- NULL 
+    d$t <- NULL 
+    d$animal <- NULL 
+    d$y <- droplevels(d$y)
+    
+    rf <- randomForest(y ~ ., d, ntree=100, sampsize=c(1000,1000,1000))
+    plot(rf)
+    print(rf)
+    imp_df <- data.frame(importance(rf))
+    bad_vars <- subset(imp_df, MeanDecreaseGini < quantile(rf$importance,t))
+    match_bad_name = match(rownames(bad_vars), colnames(df))
+    return(subset(df, select = -match_bad_name))
+
+    
+    
+    }
+
+test_different_lags <- function(tau, df){
+    print("==========================")
+    print(paste("lag =", tau))
+    anim_df = split(df,df$animal)
+    lagged_anim_dfs <- lapply(anim_df, add_lagged_time_features,  min_max_lag=c(-tau,+tau))
+    df <- do.call("rbind",lagged_anim_dfs)
+
+    #remove ambiguous vigilance states
+    df <- subset( df, py == 1)
+    df$py <- NULL 
+    df$y <- droplevels(df$y)
+    df$treatment <- NULL 
+    
+    print(dim(df))
+#~     l = sapply(levels(df$animal), crossval_test, original_df=df)
+    cl <- makeCluster(4)
+    clusterExport(cl, "randomForest")
+    clusterExport(cl, "entropy")
+    clusterExport(cl, "confusionMatrix")
+    l = parLapply(cl, levels(df$animal), crossval_test, original_df=df)
+    stopCluster(cl)
+    ms <- lapply(l, function(e) {
+        t(e$byClass)
+        })
+    print(apply(abind(ms, along=3), c(1,2), mean))
+#~     cm_mean <- apply(abind(l["byClass",], along=3), c(1,2), mean)
+#~     cm_sd <- apply(abind(l["byClass",], along=3), c(1,2), sd)
+    
+    return(l)
+}
+
+"
+Starts here
+"
+
+
+dfo <- read.csv(OUT_CSV , na.string="NaN")
+#dfo <- readRDS(file="/data/pyrem/Ellys/all_features.rds")
+
+df <- curate_df(dfo)
+
+
+print("Selecting variables")
+#~ df <- select_variables(df)
+
+l <- lapply(0:10, test_different_lags, df)
+
+stop()
 ############################################################################
 #~
 #df <- subset(df, df$vigil_value==87)
