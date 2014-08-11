23034eb24d49777c7b0b7015dbaf33082364c8fd
Quentin Geissmann <qgeissmann@gmail.com>
2014-08-11 13:51:18 +0100

    correct bug in crosvallidation -> overfit; analysis of relation between confidence and crossvalidation error

--- prototype/R/clean_rf.R
+++ prototype/R/clean_rf.R
@@ -17,14 +17,40 @@
 
 ######################################################
 rm(list = ls())
+graphics.off()
 set.seed(12134)
 library(markovchain)
 library(randomForest)
 library("abind")
+library("parallel")
 ######################################################
 OUT_PREFIX<- "/data/pyrem/Ellys/all_features"
+VARIABLE_TO_USE <-c(
+	"EEG_parietal_frontal_cD_1.power.mean",        
+	"EEG_parietal_frontal_cD_1.power.min",        
+	
+	"EEG_parietal_frontal_cD_6.power.mean",        
+	"EEG_parietal_frontal_cD_6.power.min",        
+	
+	"EEG_parietal_frontal_cD_5.hjorth.complexity",   
+	"EEG_parietal_frontal_cD_5.hjorth.morbidity",
+	
+	"EEG_parietal_frontal_cD_5.power.mean",
+	"EEG_parietal_frontal_cD_5.power.min",
+	
+	"EMG_REF_cD_1.power.min",                                          
+	"EMG_REF_cD_1.power.mean",                                          
+	"EMG_REF_cD_2.power.min",                       
+	"EMG_REF_cD_2.power.mean",                       
+	"EMG_REF_cD_3.power.min",
+	"EMG_REF_cD_3.power.mean"
+)
 
-
+entropy_conf <- function(v){
+    v[v==0] <- 1e-100
+    h <- 1 + sum(v * log2(v)) /log2(length(v))
+    return(h)
+}
 
 curate_df <- function(df){
     
@@ -50,9 +76,6 @@ curate_df <- function(df){
     return(df)
 }
 
-
-
-
 cache_load_file <- function(prefix){
 	out_csv = paste(prefix, ".csv",sep="")
 	out_rds= paste(prefix, ".rds",sep="")
@@ -65,7 +88,6 @@ cache_load_file <- function(prefix){
 		return(df)
 	}
 }
-
 
 to_homogenous_univariate_ts <- function(df){
 	
@@ -110,36 +132,86 @@ strip_df_for_ml <- function(df){
 	return(d)
 	}
 	
-stratified_cv_one_level <- function(out_level, df, rf){
+stratified_cv_one_level <- function(out_level, df, rf_fun, return_preds = FALSE){
 	test_df <- subset(df, animal ==out_level)
-	sample_size <- min(table(test_df$y))
+	train_df <- subset(df, animal !=out_level)
+	test_df <- strip_df_for_ml(test_df)
+	sample_size <- 2000
+	
 	test_classes <- split( test_df, test_df$y)
 	test_classes_l <- lapply(test_classes, function(d){
-		idxs <- sample(1: nrow(d))
+		idxs <- sample(1: nrow(d), sample_size, replace=T)
 		return(d[idxs,])
 		})
+
 	test_df <- do.call("rbind", test_classes_l)
-	test_df <- strip_df_for_ml(test_df)
+	rf <- rf_fun(train_df)
+
+	
+	if(return_preds){
+		preds_p <- predict(rf, test_df, type="prob")
+		conf <- apply(preds_p, 1, entropy_conf)
+		preds <- apply(preds_p, 1, which.max)
+		preds <- levels(test_df$y)[preds]
+		out_df <- data.frame(y= test_df$y, preds = preds, conf, animal=out_level)
+		return(out_df)
+	}
 	
 	preds <- predict(rf, test_df)
 	error <- sum(test_df$y != preds) / length(preds)
-	return(error)
+	
+	
+	return(list(error=error, rf=rf))
 }
 
-stratified_cv <- function(df, rf, nprocesses=1){
+
+
+stratified_cv <- function(df, rf_fun, return_preds=FALSE, nprocesses=4){
+	
 	if(nprocesses < 2){
-		return(mean(sapply(levels(df$animal), stratified_cv_one_level, df, rf)))
+#~ 		if(! return_rfs){
+		errors <- sapply(levels(df$animal), function(x)stratified_cv_one_level(x, df, rf_fun)$error)
+#~ 		}
+#~ 		else{
+#~ 			errors_rfs <- lapply(levels(df$animal), stratified_cv_one_level,df, rf_fun)
+#~ 			mean_error <- mean(sapply(errors_rfs,function(l)l$error))
+#~ 			rfs <- lapply(errors_rfs,function(l)l$rf)
+#~ 		}
 	}
+	else{
+		cl <- makeCluster(nprocesses)
+		clusterExport(cl, "randomForest")
+		clusterExport(cl, "stratified_cv_one_level")
+		clusterExport(cl, "strip_df_for_ml")
+		clusterExport(cl, "entropy_conf")
+		clusterExport(cl, "rf_fun")
+		if(! return_preds)
+			out <- parSapply(cl, levels(df$animal), function(x)stratified_cv_one_level(x, df, rf_fun)$error)
+		else{
+			dfs <- parLapply(cl, levels(df$animal), function(x)stratified_cv_one_level(x, df, rf_fun,return_preds))
+			out <- do.call("rbind", dfs)
+		}
+			
+		stopCluster(cl)
+		
+	}
+	
+	return(out)
 }
 select_variables <- function(df, scale=1.2){
-	d <- strip_df_for_ml(df)
+	
 	# importantly!! importance has to be computed on a balanced sample
-    rf <- randomForest(y ~ ., d, ntree=50, sampsize=c(1000,1000,1000))
-    row_names <- gsub("sample_2_0\\.", "sample_2_0d",rownames(rf$importance))
-    cv_error <- stratified_cv(df, rf)
-    imp_df <- data.frame(do.call("rbind",strsplit(row_names, "\\.")))
-    imp_df$y <- rf$importance[,1]
-    imp_df$var_name <- rownames(rf$importance)
+    rf_fun <- function(dd){
+				d <- strip_df_for_ml(dd);
+				return(randomForest(y ~ ., d, ntree=50, sampsize=c(1000,1000,1000)))
+				}
+
+    cv_error_vec <- stratified_cv(df, rf_fun)
+    
+    
+    rf <- rf_fun(df)
+
+	row_names <- gsub("sample_2_0\\.", "sample_2_0d",rownames(rf$importance))
 	t <- quantile(rf$importance, 1-1/scale)
 	
     var_to_remove <- names(rf$importance[rf$importance < t,])
@@ -147,34 +219,46 @@ select_variables <- function(df, scale=1.2){
 	if(length(bad_idxs) == 0)
 		return(NULL)
     out <- df[,-bad_idxs]
-    return(list(data=out, cv_error=cv_error, n_vars= ncol(d) - 1))
-
+    
+    # hacky way to get the real number of variable used
+    d <- strip_df_for_ml(df)
+    
+    return(list(data=out, cv_error_vec=cv_error_vec, n_vars= ncol(d) - 1))
 }
 
 recursive_variable_elimination <- function (df, scale=1.2,target=2){
 	
-
-	error = numeric()
-	n_vars = numeric()
 	variables = list()
 	i=1
 	while(TRUE){
+		
 		selection <- select_variables(df, scale=1.3)
 		if (is.null(selection)){
 			break
 		}
-		error <- c(error, selection$cv_error)
-		n_vars <- c(n_vars, selection$n_vars)
+		selection$select_variables
+		n_vars <- selection$n_vars
+		out_tmp <- data.frame(error = selection$cv_error_vec, 
+							n_vars = n_vars,
+							animal=names(selection$cv_error_vec))
+		
+		if(exists("out_df"))
+			out_df <- rbind(out_df, out_tmp)
+		else
+			out_df <- out_tmp
+		
 		cn <- colnames(df)
 		df <- selection$data
 		variables[[i]] <- cn
+		
 		i <- i + 1
-		print(i)
+		
 		if(min(n_vars) <= target){
 			break
 		}
+		print(out_df)
 	}
-	return(list(data = data.frame(n_vars, error), variables))
+	return(list(data = out_df, variables))
 }
 make_time_features <- function(lag, d, min_max_lag){
     
@@ -240,48 +324,85 @@ add_lagged_time_features_to_whole <- function(df,min_max_lag=c(-2,2),pattern_val
 	}
 
 analize_error_vs_lag <- function(df, max_tau, replicates=3){
-	taus = numeric()
-	errors = numeric()
+
 	for (i in 1:replicates){
 		for (tau in 0:max_tau){
 			dd <- add_lagged_time_features_to_whole(df, min_max_lag=c(-tau, +tau))
-			d <- strip_df_for_ml(dd)
+			
 		# importantly!! importance has to be computed on a balanced sample
-			rf <- randomForest(y ~ ., d, ntree=50, sampsize=c(1000,1000,1000))
-			print(ncol(d) - 1)
-			taus <- c(taus,tau)
-			errors <- c(errors, stratified_cv(dd, rf))
+			rf_fun <- function(dd){
+				d <- strip_df_for_ml(dd);
+				return(randomForest(y ~ ., d, ntree=50, sampsize=c(1000,1000,1000)))
+				}
+			
+			errors <- stratified_cv(dd, rf_fun)
+			if(exists("error_lag_df"))
+				error_lag_df <- rbind(error_lag_df, data.frame(error=errors, lag=tau))
+			else
+				error_lag_df <- data.frame(error=errors, lag=tau)
+			
 		}
 
 	}
-	error_lag_df <- data.frame(error=errors, lag=taus)
-	plot(error ~ lag, error_lag_df, pch=20)
+	 
+	boxplot(error ~ lag, error_lag_df, pch=20, xlab="tau", ylab=c("Average cross validation class error"),ylab=c(0,0.15))
 	return(error_lag_df)
 }
 	
 analize_error_vs_nvar <- function(df, replicates=3){
 	l <- lapply(1:replicates, function(dummy, df)recursive_variable_elimination(df)$data, df)
 	error_nvar_df <- do.call("rbind", l)
-	plot(error ~ n_vars, error_nvar_df, pch=20,log="x")
+	boxplot(error ~ n_vars, error_nvar_df, pch=20, xlab=expression(italic(p)), ylab=c("Average cross validation class error"),ylab=c(0,0.15))
 	return(error_nvar_df)
 	}
 	
+# makes a  new df where y is predicted y. prediction is realised through CV
 
+analize_error_vs_confidence <- function(df){
+	rf_fun <- function(dd){
+		d <- strip_df_for_ml(dd);
+		# 200 trees for accuracy of confidence
+		return(randomForest(y ~ ., d, ntree=200, sampsize=c(1000,1000,1000)))
+		}
+	dd <- stratified_cv(df, rf_fun, TRUE)
+	
+	conf_bins <- 0:9/10
+	error_vs_conf <- sapply(0:9/10, function(c, d){
+		sub_d <- subset(d, conf>c &  conf <= c+0.1)
+		errors <- sub_d$y != sub_d$preds
+		return(sum(errors) / nrow(sub_d))
+		}, dd)
+	barplot(error_vs_conf, width=1,space=0, names.arg=conf_bins, ylim=c(0,0.5), ylab= "Actual error", xlab="Prediction confidence")
+	hist(dd$conf, nclass=20, col="grey", freq=F, xlab="Prediction confidence", main="")
+}
+make_cv_predictions <- function(df){
+	
+	}
+
+REPLICATES <- 1
 #~ main <- function(){
+	pdf("/tmp/todel.pdf")
 	df <- cache_load_file(OUT_PREFIX)
 	dfo <- curate_df(df)
 	
-	describe_time_series(dfo)
-	error_nvar_df <- analize_error_vs_nvar(dfo,replicates=10)
- 	
-	out <- recursive_variable_elimination(dfo, target=25)
-	
-    df <- subset(dfo, select = out[[2]][[length(out[[2]])]])
+#~ 	describe_time_series(dfo)
+#~ 	error_nvar_df <- analize_error_vs_nvar(dfo,replicates=REPLICATES)
+#~  	
+#~ 	out <- recursive_variable_elimination(dfo, target=25)
+#~ 	
+#~ 	
+	non_predictor_cols <- grep("\\.", colnames(dfo), invert=T, value=TRUE)
+	df <- dfo[,c(VARIABLE_TO_USE, non_predictor_cols)]
+#~     error_lag_df <- analize_error_vs_lag(df, 2, replicates=REPLICATES)
+#~     
 
-    error_lag_df <- analize_error_vs_lag(df, 4, replicates=10)
+	analize_error_vs_confidence(df)
 
 	#todo:
-#~ 	cv_transition matrices and prevalence...
+#~ 	CV transition matrices and prevalence...
+
+#~ 	dev.off()
+
 ###finalrf:
 #~ 	dd <- add_lagged_time_features_to_whole(df, min_max_lag=c(-2, +2))
 #~ 	d <- strip_df_for_ml(dd)
@@ -290,3 +411,8 @@ analize_error_vs_nvar <- function(df, replicates=3){
 #~ 	
 #~ }
 
+
+
+
+#sleep stages? within non-rem?
+#~ plot3d(x=log10(df$EMG_REF_cD_1.power.mean), y=log10(df$EEG_parietal_frontal_cD_6.power.mean), z=log10(df$EEG_parietal_frontal_cD_1.power.mean), col=df$y)
